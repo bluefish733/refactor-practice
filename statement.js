3546c0cb49be0ad59bda8e460f080db77a68cd27
zjl <bluefish733@gmail.com>
2021-01-16 22:04:58 +0800

    replace temp with query

--- statement.js
+++ statement.js
@@ -18,26 +18,31 @@ const invoiceGlobal = {
   }],
 };
 
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function playFor(aPerformance) {
+  return playsGlobal[aPerformance.playID];
+}
+
+// 用aPerformance替代perf，由于js是动态类型语言，跟踪变量的类型很有必要
+function amountFor(aPerformance, play) {
+  let result = 0;
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 function statement(invoice, plays) {
@@ -47,21 +52,20 @@ function statement(invoice, plays) {
   const { format } = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 });
   // eslint-disable-next-line no-restricted-syntax
   for (const perf of invoice.performances) {
-    const play = plays[perf.playID];
-    const thisAmount = amountFor(perf, play);
+    const thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n}`;
   result += `You earned ${volumeCredits} credits\n`;
-  console.log(result);
   return result;
 }
 
-statement(invoiceGlobal, playsGlobal);
+const result = statement(invoiceGlobal, playsGlobal);
+console.log(result);
